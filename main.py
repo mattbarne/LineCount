959f084dc6147b21e5bf123de190673c20c03be3
Matt <106892319+mattbarne@users.noreply.github.com>
2025-04-15 20:45:25 -0500

    Updated function due to CSV time error

--- main.py
+++ main.py
@@ -25,10 +25,27 @@ if uploadFile:
     # Get list of unique pick dates
     uniqueDates = dataFrame['Pick Date'].unique()
 
-    # Shift filter function
+    # Updated shift filter function
     def filterByShift(shift, dateFilter=None):
-        if shift == 'Dayshift':
-            # 07:00 to 19:00 on a given day
+        if shift == 'Nightshift':
+            if not dateFilter:
+                return pd.DataFrame()
+
+            # Convert the dateFilter into datetime objects
+            nightStart = pd.to_datetime(dateFilter + ' 19:00:00')
+            nextDay = (pd.to_datetime(dateFilter) + pd.Timedelta(days=1)).strftime('%m/%d/%Y')
+            nightMid = pd.to_datetime(nextDay + ' 00:00:00')
+            nightEnd = pd.to_datetime(nextDay + ' 07:00:00')
+
+            shiftData = dataFrame[
+                ((dataFrame['Pick DateTime'] >= nightStart) & (dataFrame['Pick DateTime'] <= pd.to_datetime(dateFilter + ' 23:59:59'))) |
+                ((dataFrame['Pick DateTime'] >= nightMid) & (dataFrame['Pick DateTime'] < nightEnd))
+            ]
+
+            return shiftData
+
+        elif shift == 'Dayshift':
+            # 07:00 to 19:00 on the same day
             if dateFilter:
                 shiftData = dataFrame[
                     (dataFrame['Pick Date'] == dateFilter) &
@@ -40,30 +57,6 @@ if uploadFile:
                     (dataFrame['Pick DateTime'].dt.hour >= 7) &
                     (dataFrame['Pick DateTime'].dt.hour < 19)
                 ]
-            return shiftData
-
-        elif shift == 'Nightshift':
-            # 19:00 current day to 07:00 next day
-            if not dateFilter:
-                return pd.DataFrame()
-
-            nightStart = pd.to_datetime(dateFilter + ' 19:00:00')
-            nightEnd = nightStart + pd.Timedelta(hours=12)
-
-            morningStart = pd.to_datetime(dateFilter + ' 00:00:00')
-            morningEnd = pd.to_datetime(dateFilter + ' 07:00:00')
-
-            morningOffPickers = dataFrame[
-                (dataFrame['Pick DateTime'] >= morningStart) &
-                (dataFrame['Pick DateTime'] < morningEnd)
-            ]['Picked By'].unique()
-
-            shiftData = dataFrame[
-                (dataFrame['Pick DateTime'] >= nightStart) &
-                (dataFrame['Pick DateTime'] < nightEnd) &
-                (~dataFrame['Picked By'].isin(morningOffPickers))
-            ]
-
             return shiftData
 
     # Shift selection buttons
